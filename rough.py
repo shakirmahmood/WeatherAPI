4eaf6a4d0e6cb7aeafa7e1d35845147db388fb09
shakirmahmood <46704987+shakirmahmood@users.noreply.github.com>
2019-02-20 11:00:56 +0500

    Delete rough.py

--- rough.py
+++
@@ -1,9 +0,0 @@
-d = {"name":"Shakir",
-     "qualification":
-         {"Matric": "Siddeeq",
-              "FSC": "Global",
-              "Uni": "National"}
-     }
-
-
-print(d['qualification']["Matric"])
